e9801108a03e95e822ec7f9ce984801ad0ff49fc
Sistemas1 <emmita.992@gmail.com>
2025-05-05 15:53:29 -0300

    Se agrego ejercicio n°1 realizado

---
+++ 04 Estructuras Repetitivas/GONZALEZ_TP-04.py
@@ -0,0 +1,10 @@
+
+#Ejercicio 1
+
+#Imprime todos los números enteros desde 0 hasta 100.
+
+for i in range(0, 101):
+    print(i)
+
+##################################################################
+#Ejercicio 2
