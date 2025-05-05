3ea6c19ca6a5e91d59026ccf60d6ea681868839b
Sistemas1 <emmita.992@gmail.com>
2025-05-05 16:11:53 -0300

    Se realizo ejercicio 2

--- 04 Estructuras Repetitivas/GONZALEZ_TP-04.py
+++ 04 Estructuras Repetitivas/GONZALEZ_TP-04.py
@@ -8,3 +8,18 @@ for i in range(0, 101):
 
 ##################################################################
 #Ejercicio 2
+#Solicita al usuario un número entero y determina la cantidad de dígitos que contiene.
+
+numero=int(input("Ingrese un numero entero: "))
+cantidad_digitos=0
+
+while numero > 0:
+     numero=numero//10
+     cantidad_digitos +=1
+
+print (f"La cantidad de digitos que tiene es {cantidad_digitos}")
+
+#####################################################################
+#Ejercicio 3
+#Suma todos los números enteros comprendidos entre dos valores 
+# dados por el usuario, excluyendo esos dos valores.
